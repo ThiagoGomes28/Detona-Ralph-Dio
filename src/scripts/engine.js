c505ff86908b00e8f17efd6cb175f07064e92584
Thiago Gomes <thiago.g.sousa100@gmail.com>
2025-06-10 14:32:25 -0300

    subindo o projeto

---
+++ src/scripts/engine.js
@@ -0,0 +1,70 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        curretTime: 60,
+    },
+    actions: {
+        timeId: null,
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+};
+
+function countDown(){
+    state.values.curretTime--;
+    state.view.timeLeft.textContent = state.values.curretTime;
+
+    if (state.values.curretTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timeId);
+        alert("Game Over! Seu resultado foi: " + state.values.result);
+    }
+}
+
+function playSound(){
+    let audio = new Audio("./src/audios/hit.m4a");
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function moveEnemy(){
+    state.values.timeId = setInterval(randomSquare, state.values.gameVelocity);
+};
+
+function addListenerHitbox () {
+    state.view.squares.forEach((square)=> {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition){
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound();
+            }
+        });
+    });
+}
+
+function initialize() {
+    moveEnemy();
+    addListenerHitbox();
+}
+
+initialize();
